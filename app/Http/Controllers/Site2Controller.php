cbab9a43c026ceadf174383faff6e5cd689ad6de
maha1113 <mahasa304@gmail.com>
2023-07-11 15:12:26 +0300

    first commit

---
+++ app/Http/Controllers/Site2Controller.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Site2Controller extends Controller
+{
+    public function index(){
+        return view('site2assets.index');
+    }
+
+    public function about(){
+       return view('site2assets.about');
+    }
+
+
+    public function cart(){
+        return view('site2assets.cart');
+     }
+
+
+    public function checkout(){
+        return view('site2assets.checkout');
+     }
+
+
+     public function contact(){
+        return view('site2assets.contact');
+     }
+
+
+     public function index_2(){
+        return view('site2assets.index_2');
+     }
+
+     public function news(){
+        return view('site2assets.news');
+     }
+
+
+     public function shop(){
+        return view('site2assets.shop');
+     }
+
+     public function single(){
+        return view('site2assets.single');
+     }
+
+     public function singlee(){
+        return view('site2assets.news');
+     }
+
+}
+
+
---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Site2Controller;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
+Route::prefix('')->name('site2.')->group(function(){
+    Route::get('/',[Site2Controller::class, 'index'])->name('index');
+    Route::get('/about',[Site2Controller::class, 'about'])->name('about');
+    Route::get('/cart',[Site2Controller::class, 'cart'])->name('cart');
+    Route::get('/checkout',[Site2Controller::class, 'checkout'])->name('checkout');
+    Route::get('/contact',[Site2Controller::class, 'contact'])->name('contact');
+    Route::get('/index_2',[Site2Controller::class, 'index_2'])->name('index_2');
+    Route::get('/news',[Site2Controller::class, 'news'])->name('news');
+    Route::get('/shop',[Site2Controller::class, 'shop'])->name('shop');
+    Route::get('/single/news',[Site2Controller::class, 'single'])->name('single.news');
+    Route::get('/single/product',[Site2Controller::class, 'singlee'])->name('single.product');
+   });
